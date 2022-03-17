c6249ee5a6f8e6190109201a29c653c498cd6edc
Likitha prasad <94587591+Likixx05@users.noreply.github.com>
2022-03-17 04:53:09 +0000

    p1 completed

--- p1original.c
+++ p1original.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<math.h>
-void input(float *x1, float *x2, float *y1, float *y2)
+void input(float *x1, float*y1,float *x2, float *y2)
 {
-  printf("enter the first points\n");
+  printf("enter the first points:\n");
   scanf("%f%f",x1,y1);
-  printf("enter the second points\n");
+  printf("enter the second points:\n");
   scanf("%f%f",x2,y2);
 }
 void find_distance(float x1,float y1,float x2,float y2,float *distance)
